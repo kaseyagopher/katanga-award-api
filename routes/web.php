47238f2855639e2e4189ed7afa94dacf198a2834
kaseyagopher <alghufarsanjab@gmail.com>
2025-10-03 14:07:58 +0100

    ajoute du prefixe katanga-award

--- routes/web.php
+++ routes/web.php
@@ -12,39 +12,42 @@ use App\Http\Controllers\ResultatController;
 use App\Http\Controllers\CategorieController;
 use App\Http\Controllers\VoteSummaryController;
 
-Route::get('/', function () {
-    return to_route('login');
-});
+Route::prefix('katanga-award')->group(function () {
 
-Route::get('/login', [AuthController::class, 'index'])->name('login');
-Route::post('/login',[AuthController::class, 'loginUser']);
+    Route::get('/', function () {
+        return to_route('login');
+    });
 
-Route::get('/loginAdmin',[AuthController::class, 'admin'])->name('admin.login');
-Route::post('/loginAdmin',[AuthController::class, 'loginAdmin']);
-Route::get('/logout', [AuthController::class,'user_logout'])->name('user.logout');
+    Route::get('/login', [AuthController::class, 'index'])->name('login');
+    Route::post('/login',[AuthController::class, 'loginUser']);
 
-Route::get('/logout',[AuthController::class, 'logout'])->name('admin.logout');
+    Route::get('/loginAdmin',[AuthController::class, 'admin'])->name('admin.login');
+    Route::post('/loginAdmin',[AuthController::class, 'loginAdmin']);
 
-Route::prefix('admin')->group(function () {
-    Route::get('/dashboard',[AdminController::class, 'index'])->name('admin.dashboard');
-    Route::resource('editions', EditionController::class);
-    Route::resource('categories', CategorieController::class);
-    Route::resource('candidats', CandidatController::class);
-    Route::get('/resultats', [ResultatController::class, 'index'])->name('resultats.index');
-    Route::get('/resultats/data', [ResultatController::class, 'data'])->name('resultats.data');
-});
+    Route::get('/logout', [AuthController::class,'user_logout'])->name('user.logout');
+    Route::get('/logout',[AuthController::class, 'logout'])->name('admin.logout');
 
-Route::prefix('user')->group(function(){
-    Route::get('/',[UserController::class, 'index'])->name('user.index');
-    Route::get('/apropos',[UserController::class, 'user_apropos'])->name('user.apropos');
-    Route::get('/vote',[UserController::class, 'vote'])->name('user.vote');
-    Route::post('/vote',[VoteController::class, 'store'])->name('vote.store');
-    Route::get('/vote/summary', [VoteSummaryController::class, 'show'])
-     ->name('vote.summary')
-     ->middleware('auth:web');
+    Route::prefix('admin')->group(function () {
+        Route::get('/dashboard',[AdminController::class, 'index'])->name('admin.dashboard');
+        Route::resource('editions', EditionController::class);
+        Route::resource('categories', CategorieController::class);
+        Route::resource('candidats', CandidatController::class);
+        Route::get('/resultats', [ResultatController::class, 'index'])->name('resultats.index');
+        Route::get('/resultats/data', [ResultatController::class, 'data'])->name('resultats.data');
+    });
 
-});
+    Route::prefix('user')->group(function(){
+        Route::get('/',[UserController::class, 'index'])->name('user.index');
+        Route::get('/apropos',[UserController::class, 'user_apropos'])->name('user.apropos');
+        Route::get('/vote',[UserController::class, 'vote'])->name('user.vote');
+        Route::post('/vote',[VoteController::class, 'store'])->name('vote.store');
+        Route::get('/vote/summary', [VoteSummaryController::class, 'show'])
+            ->name('vote.summary')
+            ->middleware('auth:web');
+    });
 
-Route::fallback(function () {
-    return response()->view('errors.404', [], 404);
+    Route::fallback(function () {
+        return response()->view('errors.404', [], 404);
+    });
+
 });
